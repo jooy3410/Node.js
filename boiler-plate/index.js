edbadb8929a22eb9d008540321ddc98cc131552d
jooy3410 <jooy3410@naver.com>
2022-07-17 08:45:38 +0900

    mongodb구축 후 연동, 스키마 작성

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017')
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
---
+++ boiler-plate/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+//스키마
+const User = mongoose.model('User', userSchema)
+
+//다른곳에서도 쓸 수 있게 exports해준다
+module.exports = { User}
