33996997c70b59688affc9c08f2b1bf244366770
jooy3410 <jooy3410@naver.com>
2022-08-03 22:09:11 +0900

    리액트 동작구조

--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//public폴더의 index.html에 보면  id가 root인것이 보이는데 그것에다가 보여줄 것이라고 정의를 해주는 곳이다.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
