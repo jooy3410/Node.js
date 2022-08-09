3dfdaef2bf23502e88e73ef01366ce1180cccee3
jooy3410 <jooy3410@naver.com>
2022-08-09 23:26:19 +0900

    es7연습

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -9,3 +9,17 @@ function LandingPage() {
 }
 
 export default LandingPage
+
+import React from 'react'
+import React, { Component } from 'react'
+
+
+import React, { Component } from 'react'
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>LandingPage</div>
+    )
+  }
+}
